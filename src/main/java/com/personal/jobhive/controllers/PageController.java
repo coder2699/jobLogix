0963e926b5619b24dc8d5cbfbd35aa82a4dd9730
coder2699 <pranshu0414@gmail.com>
2024-09-02 02:06:55 +0530

    thymeleaf base setup

--- src/main/java/com/personal/jobhive/controllers/PageController.java
+++ src/main/java/com/personal/jobhive/controllers/PageController.java
@@ -13,4 +13,19 @@ public class PageController {
         model.addAttribute("githubRepo", "https://github.com/coder2699/");
         return "home";
     }
+
+    // about route
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // services
+    @GetMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
 }
