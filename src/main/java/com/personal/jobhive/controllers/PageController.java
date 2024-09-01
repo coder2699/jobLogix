5a5e1ccd4912934fe7c2db3bd4facb86b504be7c
coder2699 <pranshu0414@gmail.com>
2024-09-02 01:58:41 +0530

    tailwind setup

--- src/main/java/com/personal/jobhive/controllers/PageController.java
+++ src/main/java/com/personal/jobhive/controllers/PageController.java
@@ -1,13 +1,16 @@
 package com.personal.jobhive.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class PageController {
     @GetMapping("/home")
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("name", "Pranshu Pandey");
+        model.addAttribute("githubRepo", "https://github.com/coder2699/");
         return "home";
     }
 }
