810f53158d8801d2ece969e12c985e546b7b3ad2
coder2699 <pranshu0414@gmail.com>
2024-09-02 02:26:25 +0530

    login, signup, dark mode setup

--- src/main/java/com/personal/jobhive/controllers/PageController.java
+++ src/main/java/com/personal/jobhive/controllers/PageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
     @GetMapping("/home")
-    public String getHome(Model model) {
+    public String homePage(Model model) {
         model.addAttribute("name", "Pranshu Pandey");
         model.addAttribute("githubRepo", "https://github.com/coder2699/");
         return "home";
@@ -27,5 +27,26 @@ public class PageController {
     public String servicesPage() {
         System.out.println("services page loading");
         return "services";
+    }
+
+    // job
+    @GetMapping("/job")
+    public String jobPage() {
+        System.out.println("job page loading");
+        return "job";
+    }
+
+    // login
+    @GetMapping("/login")
+    public String loginPage() {
+        System.out.println("login page loading");
+        return "login";
+    }
+
+    // register
+    @GetMapping("/register")
+    public String registerPage() {
+        System.out.println("register page loading");
+        return "register";
     }
 }
